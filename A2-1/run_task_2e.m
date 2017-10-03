f920c40cf17769c7f0c9e898dec79338168f4c0f
HeleneHF <helenehf@stud.ntnu.no>
2017-10-03 18:55:20 +0200

    tirsdag

--- A2-1/run_task_2e.m
+++ A2-1/run_task_2e.m
@@ -2,16 +2,37 @@
 
 %% Constants
 
-a1 = 2.87;
-a2 = -0.65;
+% PI for course angle chi
+K_p_chi = 1; % Change later!!
+K_i_chi = 1; % Change later!!
 
-kp = -5/3;
+% PID for phi
+K_p_phi = -5/3; 
+K_i_phi = -0; % Change later!
+
+% Calculating K_d_phi:
 w = sqrt(a2*kp);
 z = 0.7;
 
 f_kd = @(zeta, wn) (2*zeta*wn - a1)/a2;
 
-kd = f_kd(z, w);
+K_d_phi = f_kd(z, w);
+
+% Aileron
+a1 = 2.87;
+a2 = -0.65;
+
+g = 9.81; % [m/s^2] gravity
+V_g = 637/3.6 ; % [m/s] Speed of ground (637 [km/h])  
+d = 2; 
+
+% % Div
+% w = sqrt(a2*kp);
+% z = 0.7;
+% 
+% f_kd = @(zeta, wn) (2*zeta*wn - a1)/a2;
+% 
+% kd = f_kd(z, w);
 
 %% Define transfer function
 
@@ -20,7 +41,6 @@ H = tf( -a2/( s *(s^2 + (a1+a2*kd)*s + a2*kp ) ) );
 rlocus(H);
 
 %% Choose ki and find poles
-ki = -0.0; %Change later
 sys = tf( a2*kp*(s+ki/kp)  /  ( s^3  + s^2*(a1+a2*kd) + s*(a2*kp) + a2*ki )  );
 pole(sys)
 bode(sys); grid on;
