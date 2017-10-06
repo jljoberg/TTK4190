a9f051f44c96bdaa017dbd26ca0b7b65812c9b6e
HeleneHF <helenehf@stud.ntnu.no>
2017-10-06 17:46:35 +0200

    improved plot functions, figures for report

--- A2-1/Task_2g/plot_g.m
+++ A2-1/Task_2g/plot_g.m
@@ -1,10 +1,11 @@
 %% nonlinear model: Saturated vs. non-saturated
-subplot(2,1,1)
+figure
+subplot(2,1,1); hold on; 
 plot(chi.time, chi.data(:,2), '--', 'Color', 'r'); plot(chi_sat.time, chi_sat.data(:,2), 'b');
 legend('\chi_{non-saturated}', '\chi_{saturated}'); xlabel('Time [s]'); ylabel('Angle [deg]'); title('Course angle, non linear')
 xlim([0,230])
 
-subplot(2,1,2)
-plot(chi.time, chi.data(:,3)); plot(chi_sat.time, chi_sat.data(:,3),'b'); 
-legend('\delta_a','\delta_a_sat'); xlabel('Time [s]'); ylabel('Angle [deg]'); title('Aileron angle')
+subplot(2,1,2);hold on; 
+plot(chi.time, chi.data(:,3),'--','Color','r'); plot(chi_sat.time, chi_sat.data(:,3),'b'); 
+legend('\delta_{a_{non-sat}}','\delta_{a_{sat}}'); xlabel('Time [s]'); ylabel('Angle [deg]'); title('Aileron angle')
 ylim([-30 30]); xlim([0,230])
